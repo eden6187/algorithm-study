106e0e3cc3627a37719e9e6e3cff6ee558d8f39c
Younghyun Kim <kyh970107@ajou.ac.kr>
2021-07-23 23:43:10 +0900

    [20210723] - README.md 에 내용 추가

---
+++ src/baekjoon/boj10825/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.boj10825;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.solve();
+    }
+
+    public void solve(){
+
+    }
+
+//    class Student{
+//        String name;
+//        int
+//    }
+}
